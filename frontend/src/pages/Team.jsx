7ab7bfd361b165be8d1dd5ac8413862cc4dd4f6b
krishan2005op <krishanmalhotra2005@gmail.com>
2024-08-10 22:07:44 +0530

    testing

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -5,6 +5,7 @@ const Team = () => {
     <div className=' flex justify-center items-center h-[90vh]'>
         <div className="grid grid-cols-2">
             <h1 className='text-8xl font-extrabold ml-20 text-gray-800'>OUR MEMBERS</h1>
+            <h1>krishan</h1>
         </div>
     </div>
   )
