88fb35b6d6ddf204a2b9542728c30acd2cf422bb
krishan2005op <krishanmalhotra2005@gmail.com>
2024-08-11 02:30:35 +0530

    testing

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import './style/team.css'
 
 const Team = () => {
   return (
-    <div className=' flex justify-center items-center h-[90vh]'>
-        <div className="grid grid-cols-2">
-            <h1 className='text-8xl font-extrabold ml-20 text-gray-800'>OUR MEMBERS</h1>
-            <h1>krishan</h1>
-        </div>
+  <>
+      {/* // <div className=' flex justify-center ml-20 mt-16 h-[90vh]'>
+    //     <div className="grid grid-cols-2">
+    //         <h1 className='text-8xl font-extrabold ml-20 text-gray-800'>OUR MEMBERS</h1>
+    //     </div>
+    // </div> */}
+    <div className='content'>
+      <div className=''>
+        <p>About our Past members</p>
+        <h1>Team of GDSC Member</h1>
+        <button>Meet The team</button>
+      </div>
     </div>
+
+    <div className='content1'>
+      <div>
+        <button>Random Text</button>
+        <h2>Executive Board</h2>
+        <p>
+          Explore our diverse range of projects that showcase
+          the creativity and technical prowess of our members.
+          From groundbreaking apps to innovate solutions, our
+          project highlight the impact of applied technology.
+        </p>
+      </div>
+    </div>
+  </>
   )
 }
 
