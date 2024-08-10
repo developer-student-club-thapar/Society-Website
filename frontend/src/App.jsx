32108d418915fed02459e252eb526e7ec821e247
Aayush <agoyal1_be23@thapar.edu>
2024-08-10 12:14:55 +0530

    main page changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,24 @@ import Home from "./pages/Home";
 import Team from "./pages/Team";
 import Projects from "./pages/Projects";
 import Events from "./pages/Events";
-import Test from './pages/Test';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Departments from "./pages/Departments";
+import Alumni from "./pages/Alumni";
 
 function App() {
+
   return (
     <div className=" w-full min-h-screen "> 
       <BrowserRouter>
         <Navbar />
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="/trial" element={<Test />}></Route>
-            <Route path="/team" element={<Team />}></Route>
             <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/departments" element={<Departments />}></Route>
             <Route path="/events" element={<Events />}></Route>
+            <Route path="/alumni" element={<Alumni />}></Route>
+            <Route path="/team" element={<Team />}></Route>
           </Routes>
         <Footer />
       </BrowserRouter>
