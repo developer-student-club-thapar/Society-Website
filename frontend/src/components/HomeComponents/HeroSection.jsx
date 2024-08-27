fd338b627bb2e082133eeca42b0242083c02faab
Aayush <agoyal1_be23@thapar.edu>
2024-08-27 14:31:42 +0530

    main page changes

--- frontend/src/components/HomeComponents/HeroSection.jsx
+++ frontend/src/components/HomeComponents/HeroSection.jsx
@@ -11,8 +11,8 @@ const HeroSection = () => {
         <img src={heroImage} alt="hero" className=' absolute -z-20 heroimg' />
         
         <div className=" w-[80%] justify-center items-center flex flex-col">
-            <h2 className='text-gray-600 font-light mb-4 mt-8'>Google Developer Students Club Thapar University</h2>
-            <h1 className=' text-3xl lg:text-8xl text-center font-bold text-gray-900'>Empower Your Future With Innovation</h1>
+            <h2 className='text-gray-600 font-light mb-4 mt-8 lg:text-sm text-xs text-center'>Google Developer Students Club Thapar University</h2>
+            <h1 className=' text-2xl lg:mt-0 mt-2 lg:text-8xl text-center font-bold text-gray-900'>Empower Your Future With Innovation</h1>
             <button className='bg-black px-16 py-3 rounded-3xl mt-10 text-white hover:bg-white  hover:text-black transition-all hover:border-2 hover:border-black'>Learn More</button>
        </div>
     </div>
