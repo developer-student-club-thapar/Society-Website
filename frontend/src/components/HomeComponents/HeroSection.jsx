040a59b235c7232512dea8bf46e248c0d0539f3e
Aayush <agoyal1_be23@thapar.edu>
2024-09-02 16:28:23 +0530

    main page

--- frontend/src/components/HomeComponents/HeroSection.jsx
+++ frontend/src/components/HomeComponents/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import heroImage from '../../assets/Hero-Shapes.png'
+import mobbg from '../../assets/mob_bg.png'
 
 
 
@@ -7,11 +8,12 @@ import heroImage from '../../assets/Hero-Shapes.png'
 const HeroSection = () => {
   return (
     <div className='flex justify-center items-center flex-col h-[80vh] '>
-        <img src={heroImage} alt="hero" className=' absolute -z-20 heroimg' />
+        <img src={heroImage} alt="hero" className=' lg:block hidden absolute -z-20 heroimg' />
+        <img src={mobbg} alt="hero" className='lg:hidden block absolute -z-20 heroimg' />
         
         <div className=" w-[80%] justify-center items-center flex flex-col">
-            <h2 className='text-gray-600 font-light mb-4 mt-8 lg:text-sm text-xs text-center'>Google Developer Students Club Thapar University</h2>
-            <h1 className=' text-2xl lg:mt-0 mt-2 lg:text-8xl text-center font-bold text-gray-900'>Empower Your Future With Innovation</h1>
+            <h2 className='text-gray-600 font-light lg:mb-4 mt-8 lg:text-sm text-[12px] text-center'>Google Developer Students Club Thapar University</h2>
+            <h1 className=' text-4xl lg:mt-0 mt-2 lg:text-8xl text-center font-bold text-gray-900'>Empower Your Future With Innovation</h1>
             <button className='bg-black px-16 py-3 rounded-3xl mt-10 text-white hover:bg-white  hover:text-black transition-all hover:border-2 hover:border-black'>Learn More</button>
        </div>
     </div>
